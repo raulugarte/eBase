fcfbceefd9fc0e94094924cc5e79790a40f9deb8
raulugarte <63106469+raulugarte@users.noreply.github.com>
2023-09-09 08:48:37 +0200

    Update contentfragmentlist.js

--- blocks/contentfragmentlist/contentfragmentlist.js
+++ blocks/contentfragmentlist/contentfragmentlist.js
@@ -117,6 +117,6 @@ function getImageUrl(image, isUE) {
         return image["_authorUrl"];
     }
     const url = new URL(image["_publishUrl"])
-    /*return `https://${url.hostname}${image["_dynamicUrl"]}`*/
-    return `${image["_publishUrl"]}`
+    return `https://${url.hostname}${image["_dynamicUrl"]}`
+    /*return `${image["_publishUrl"]}`*/
 }
