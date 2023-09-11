a83fe90dad0c6d682d7d18a630383b454ed7d816
raulugarte <63106469+raulugarte@users.noreply.github.com>
2023-09-11 13:00:02 +0200

    Update contentfragmentlist.js

--- blocks/contentfragmentlist/contentfragmentlist.js
+++ blocks/contentfragmentlist/contentfragmentlist.js
@@ -15,8 +15,8 @@ export default async function decorate(block) {
         elem.innerHTML = `
             <div class="category-item-image">
                 <picture>
-                    <source type="image/webp" srcset="${category.image.deliveryUrl}?preferweb=true" media="(min-width: 600px)">
-                    <source type="image/webp" srcset="${category.image.deliveryUrl}?preferweb=true&width=750">
+                    <source type="image/webp" srcset="${category.image.deliveryUrl}?preferwebp=true" media="(min-width: 600px)">
+                    <source type="image/webp" srcset="${category.image.deliveryUrl}?preferwebp=true&width=750">
                     <source type="${category.image.mimeType}" srcset="${category.image.deliveryUrl}" media="(min-width: 600px)">
                     <img src="${category.image.url}" width="${category.image.width}" height="${category.image.height}" alt="${category.title}" type="${category.image.mimeType}" itemprop="primaryImage" itemtype="image" loading="lazy">
                 </picture>
